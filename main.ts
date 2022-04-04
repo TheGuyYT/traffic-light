3b72e6f85ed5e0838f99906cbd87fcb9647df7f8
TheGuy <the.guy.ec2201220@gmail.com>
2022-04-04 11:02:14 +0800

    sideways drawing because when they are attached to the stop:bit

--- main.ts
+++ main.ts
@@ -1,43 +1,43 @@
 basic.forever(function () {
     basic.showLeds(`
-        . . # . .
-        . . # . .
-        . . # . .
         . . . . .
-        . . # . .
+        . . . . .
+        # . # # #
+        . . . . .
+        . . . . .
         `)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
     basic.pause(14000)
     basic.showLeds(`
+        . . # . .
         . . . . .
+        . . # . .
         . . . . .
-        # . # . #
-        . . . . .
-        . . . . .
+        . . # . .
         `)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
     basic.pause(1000)
     basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . # .
-        # . # . .
         . # . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . # .
         `)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
     basic.pause(14000)
     basic.showLeds(`
+        . . # . .
         . . . . .
+        . . # . .
         . . . . .
-        # . # . #
-        . . . . .
-        . . . . .
+        . . # . .
         `)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
