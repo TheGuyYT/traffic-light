5cd8635a70e9a8173e91f9ebd809ffb687884fb5
TheGuy <the.guy.ec2201220@gmail.com>
2022-04-04 11:06:17 +0800

    Restore '04/04/2022, 10:44:35 Added mode: Manual'

--- main.ts
+++ main.ts
@@ -1,47 +1,80 @@
+input.onButtonPressed(Button.A, function () {
+    mode += 1
+    if (mode > 1) {
+        mode = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (mode == 1) {
+        state += 1
+        if (state > 4) {
+            state = 1
+        }
+    }
+})
+let state = 0
+let mode = 0
+mode = 0
 basic.forever(function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # . # # #
-        . . . . .
-        . . . . .
-        `)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
+    state = 1
     basic.pause(14000)
-    basic.showLeds(`
-        . . # . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . # . .
-        `)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
+    state = 2
     basic.pause(1000)
-    basic.showLeds(`
-        . # . . .
-        # . . . .
-        . # . . .
-        . . # . .
-        . . . # .
-        `)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
+    state = 3
     basic.pause(14000)
-    basic.showLeds(`
-        . . # . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . # . .
-        `)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
+    state = 4
     basic.pause(1000)
-    basic.pause(1000)
+})
+basic.forever(function () {
+    if (mode == 2) {
+        if (state == 1) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . .
+                . . . . .
+                . . # . .
+                `)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
+        }
+        if (state == 2) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # . # . #
+                . . . . .
+                . . . . .
+                `)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
+        }
+        if (state == 3) {
+            basic.showLeds(`
+                . . . . .
+                . . . . #
+                . . . # .
+                # . # . .
+                . # . . .
+                `)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
+        }
+        if (state == 4) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # . # . #
+                . . . . .
+                . . . . .
+                `)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
+            basic.pause(1000)
+        }
+    }
 })
